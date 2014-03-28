66310cf602306a4d1cec45b5e91d01b046ac2525
Tim Spinks <tim@ninthlink.com>
2014-03-28 19:01:14 +0000

    add google tag manager

--- functions.php
+++ functions.php
@@ -337,3 +337,19 @@ function google_additional_questions_tracking(){
 	}
 }
 add_action('wp_footer', 'google_additional_questions_tracking');
+
+function google_tag_manager(){
+
+	?>
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N4K7ZS" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-N4K7ZS');</script>
+	<!-- End Google Tag Manager -->
+	<?php
+
+}
+add_action('wp_footer', 'google_tag_manager');
