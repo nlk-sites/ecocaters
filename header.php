6f2a6a0e9644747827400dcd0b5dac5ef6b95209
Tim Spinks <tim@ninthlink.com>
2015-01-21 00:05:43 +0000

    updated CSS latest live files versions

--- header.php
+++ header.php
@@ -1,53 +1,53 @@
-<?php
-/**
- * @package Eco Caters
- * @since Eco Caters 2.0
- */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
-<title><?php if ( wp_title( '', false ) ) { wp_title( '' ); } else { echo get_bloginfo( 'name' ) .' | '. get_bloginfo( 'description' ); } ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<meta property='og:locale' content='en_US' />
-<meta property='og:site_name' content='EcoCaters'/>
-<meta property='og:type' content='website'/>
-<?php
-if ( is_single() ) {
-	echo '<meta property="og:url" content="'. get_permalink() .'" />';
-	echo '<meta property="og:title" content="EcoCaters - '. get_the_title() .'" />'; 
-	if ( has_post_thumbnail() ) {
-		$imgid = get_post_thumbnail_id();
-        $imgurl = wp_get_attachment_image_src( $imgid , 'large' );
-        $imgurl = $imgurl[0];
-		echo '<meta property="og:image" content="'. $imgurl .'" />'; 
-	}
-} else {
-	echo '<meta property="og:title" content="'. esc_attr(wp_title( '', false ) ) .'" />'; 
-}
-
-
-wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="hdr">
-	<div class="hinside">
-        <a href="<?php bloginfo('url'); ?>" id="logo"><img src="<?php bloginfo('template_url'); ?>/images/2/logo.gif" alt="Eco Caters" width="193" height="92" /></a>
-        <div class="hright">
-            <div id="tagline"><?php bloginfo('description'); ?></div>
-            <p id="hlocs">Los Angeles <strong>310.202.0326</strong><br />Washington D.C. <strong>202.810.3262</strong><br />San Diego <strong>858.246.6129</strong></p>
-        </div>
-        <div class="hmid">GRASS-FED MEATS&nbsp;&nbsp;<span>|</span>&nbsp;&nbsp;LOCAL PRODUCE&nbsp;&nbsp;<span>|</span>&nbsp;&nbsp;SUSTAINABLE FOOD</div>
-	</div>
-<?php
-wp_nav_menu( array(
-                'container_id' => 'nav',
-                'theme_location' => 'mainmenu',
-                'menu_class' => false,
-            ) );
-?>
-</div>
-<div id="page">
+<?php
+/**
+ * @package Eco Caters
+ * @since Eco Caters 2.0
+ */
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
+<title><?php if ( wp_title( '', false ) ) { wp_title( '' ); } else { echo get_bloginfo( 'name' ) .' | '. get_bloginfo( 'description' ); } ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<meta property='og:locale' content='en_US' />
+<meta property='og:site_name' content='EcoCaters'/>
+<meta property='og:type' content='website'/>
+<?php
+if ( is_single() ) {
+	echo '<meta property="og:url" content="'. get_permalink() .'" />';
+	echo '<meta property="og:title" content="EcoCaters - '. get_the_title() .'" />'; 
+	if ( has_post_thumbnail() ) {
+		$imgid = get_post_thumbnail_id();
+        $imgurl = wp_get_attachment_image_src( $imgid , 'large' );
+        $imgurl = $imgurl[0];
+		echo '<meta property="og:image" content="'. $imgurl .'" />'; 
+	}
+} else {
+	echo '<meta property="og:title" content="'. esc_attr(wp_title( '', false ) ) .'" />'; 
+}
+
+
+wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="hdr">
+	<div class="hinside">
+        <a href="<?php bloginfo('url'); ?>" id="logo"><img src="<?php bloginfo('template_url'); ?>/images/2/logo.gif" alt="Eco Caters" width="193" height="92" /></a>
+        <div class="hright">
+            <div id="tagline"><?php bloginfo('description'); ?></div>
+            <p id="hlocs">Los Angeles <strong>310.202.0326</strong><br />Washington D.C. <strong>202.810.3262</strong><br />San Diego <strong>858.246.6129</strong></p>
+        </div>
+        <div class="hmid">GRASS-FED MEATS&nbsp;&nbsp;<span>|</span>&nbsp;&nbsp;LOCAL PRODUCE&nbsp;&nbsp;<span>|</span>&nbsp;&nbsp;SUSTAINABLE SEAFOOD</div>
+	</div>
+<?php
+wp_nav_menu( array(
+                'container_id' => 'nav',
+                'theme_location' => 'mainmenu',
+                'menu_class' => false,
+            ) );
+?>
+</div>
+<div id="page">
--- thank_you.php
+++ thank_you.php
@@ -1,99 +1,99 @@
-<?php
-/*
-Template Name: Thank you
-*/
-//session_start();
-
-include("contact_config.php");
-
-$sError = '';
-$sSuccess = 0;
-
-$FormAction = get_param("FormAction");
-if ($FormAction == "submit") {
-  $fldcontact_name = get_param("lf_name");
-  $fldcontact_email = get_param("lf_email");
-  $fldcontact_phone = get_param("lf_phone");
-  $phone_not_required = get_param("two_field");
-  $fldcontact_source = get_param("source");
-  
-
-  if (!strlen($fldcontact_name)) {
-    $sError .= '<b>name</b> is required field.<br>';
-  }
-  if (!strlen($fldcontact_email)) {
-    $sError .= '<b>email address</b> is required field.<br>';
-  }
-  else if (!ereg("^[a-zA-Z0-9\.\_\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$", $fldcontact_email)) {
-	$sError .= '<b>email address</b> is incorrect.<br>';
-  }
-  if (!strlen($fldcontact_phone)) {
-    $sError .= '<b>phone</b> is required field.<br>';
-  }
-
-
-/*  
-  if(!isset($_SESSION['captcha_keystring']) || $_SESSION['captcha_keystring'] != $_POST['keystring']){  
-    $sError .= '<b>code</b> is wrong.<br>'; 
-  }
-  unset($_SESSION['captcha_keystring']);
-*/  
-  if ($sError == '') {
-
-	$email_to = get_option('admin_email');
-    $message  = 'Name: '.toemail($fldcontact_name).EMAIL_EOL;
-    $message .= 'Email Address: '.toemail($fldcontact_email).EMAIL_EOL;
-    $message .= 'Phone: '.toemail($fldcontact_phone).EMAIL_EOL;
-    wp_mail('emily@ecocaters.com, anna@ecocaters.com, adam@ecocaters.com, angela@ecocaters.com, staci@ecocaters.com, john@ecocaters.com, lauren@ecocaters.com', $fldcontact_name.' contact request', $message);
-	
-	$sSuccess = 1;
-  }
-  else {
-    $fldcontact_name = strip(get_param("contact_name"));
-    $fldcontact_email = strip(get_param("contact_email"));
-    $fldcontact_phone = strip(get_param("contact_phone"));
-    $fldcontact_comment = strip(get_param("contact_comment"));
-  }
-}
-if ($sSuccess == 1) { 
-	wp_redirect(get_option('siteurl') . '/additional-questions?source='.$fldcontact_source.'&contact_name='.urlencode($fldcontact_name).'&contact_email='.urlencode($fldcontact_email).'&contact_phone='.urlencode($fldcontact_phone));
-}
-?>
-
-<?php get_header();
-
-if (have_posts()) : while (have_posts()) : the_post();
-global $post;
-eco_bgimg();
-?>
-<div class="mrap">
-	<div class="mainheader">
-    	<div class="h1"><?php eco_title(); ?></div>
-    </div>
-    <div id="main">
-        <div id="left">
-            <div id="post-<?php the_ID(); ?>">
-<?php   if ($sSuccess == 1 || strlen(get_param("source"))) { ?>
-				<?php the_content('<p class="serif">' . __('Read the rest of this page &raquo;', 'kubrick') . '</p>'); ?>
-<?php 	} else { ?>
-<?php	 if ($sError != '') { ?>
-
-	  <div class="erros"><?php echo($sError); ?></div>
-	  
-<?php	} ?>			  
-			  
-			  
-<?php 	} ?>
-	<?php endwhile; endif; ?>
-	<?php edit_post_link(__('Edit this entry.', 'kubrick'), '<p>', '</p>'); ?>
-            </div>
-        </div>
-        <div id="right">
-			<?php
-			get_sidebar('seen');
-			
-			get_sidebar('quotes');
-			?>
-        </div>
-
-<?php get_footer(); ?>
+<?php
+/*
+Template Name: Thank you
+*/
+//session_start();
+
+include("contact_config.php");
+
+$sError = '';
+$sSuccess = 0;
+
+$FormAction = get_param("FormAction");
+if ($FormAction == "submit") {
+  $fldcontact_name = get_param("lf_name");
+  $fldcontact_email = get_param("lf_email");
+  $fldcontact_phone = get_param("lf_phone");
+  $phone_not_required = get_param("two_field");
+  $fldcontact_source = get_param("source");
+  
+
+  if (!strlen($fldcontact_name)) {
+    $sError .= '<b>name</b> is required field.<br>';
+  }
+  if (!strlen($fldcontact_email)) {
+    $sError .= '<b>email address</b> is required field.<br>';
+  }
+  else if (!ereg("^[a-zA-Z0-9\.\_\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$", $fldcontact_email)) {
+	$sError .= '<b>email address</b> is incorrect.<br>';
+  }
+  if (!strlen($fldcontact_phone)) {
+    $sError .= '<b>phone</b> is required field.<br>';
+  }
+
+
+/*  
+  if(!isset($_SESSION['captcha_keystring']) || $_SESSION['captcha_keystring'] != $_POST['keystring']){  
+    $sError .= '<b>code</b> is wrong.<br>'; 
+  }
+  unset($_SESSION['captcha_keystring']);
+*/  
+  if ($sError == '') {
+
+	$email_to = get_option('admin_email');
+    $message  = 'Name: '.toemail($fldcontact_name).EMAIL_EOL;
+    $message .= 'Email Address: '.toemail($fldcontact_email).EMAIL_EOL;
+    $message .= 'Phone: '.toemail($fldcontact_phone).EMAIL_EOL;
+    wp_mail('leads@ecocaters.com, adam@ecocaters.com, nicole@ecocaters.com, emily@ecocaters.com, john@ecocaters.com', $fldcontact_name.' contact request', $message);
+	
+	$sSuccess = 1;
+  }
+  else {
+    $fldcontact_name = strip(get_param("contact_name"));
+    $fldcontact_email = strip(get_param("contact_email"));
+    $fldcontact_phone = strip(get_param("contact_phone"));
+    $fldcontact_comment = strip(get_param("contact_comment"));
+  }
+}
+if ($sSuccess == 1) { 
+	wp_redirect(get_option('siteurl') . '/additional-questions?source='.$fldcontact_source.'&contact_name='.urlencode($fldcontact_name).'&contact_email='.urlencode($fldcontact_email).'&contact_phone='.urlencode($fldcontact_phone));
+}
+?>
+
+<?php get_header();
+
+if (have_posts()) : while (have_posts()) : the_post();
+global $post;
+eco_bgimg();
+?>
+<div class="mrap">
+	<div class="mainheader">
+    	<div class="h1"><?php eco_title(); ?></div>
+    </div>
+    <div id="main">
+        <div id="left">
+            <div id="post-<?php the_ID(); ?>">
+<?php   if ($sSuccess == 1 || strlen(get_param("source"))) { ?>
+				<?php the_content('<p class="serif">' . __('Read the rest of this page &raquo;', 'kubrick') . '</p>'); ?>
+<?php 	} else { ?>
+<?php	 if ($sError != '') { ?>
+
+	  <div class="erros"><?php echo($sError); ?></div>
+	  
+<?php	} ?>			  
+			  
+			  
+<?php 	} ?>
+	<?php endwhile; endif; ?>
+	<?php edit_post_link(__('Edit this entry.', 'kubrick'), '<p>', '</p>'); ?>
+            </div>
+        </div>
+        <div id="right">
+			<?php
+			get_sidebar('seen');
+			
+			get_sidebar('quotes');
+			?>
+        </div>
+
+<?php get_footer(); ?>
